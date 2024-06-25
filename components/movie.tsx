ef52831e2032b04489cece8b58caceddd3833fc7
summermong <2514o_o@naver.com>
2024-06-25 16:38:58 +0900

    #4.2 Styling

---
+++ components/movie.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+interface IMoiveProps {
+  title: string;
+  id: string;
+  poster_path: string;
+}
+import Link from 'next/link';
+import styles from '../styles/movie.module.css';
+import { useRouter } from 'next/navigation';
+
+export default function Movie({ title, id, poster_path }: IMoiveProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.push(`/movies/${id}`);
+  };
+  return (
+    <div className={styles.movie}>
+      <img src={poster_path} alt={title} onClick={onClick} />
+      <Link href={`/movies/${id}`}>{title}</Link>
+    </div>
+  );
+}
